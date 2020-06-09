87524a3c4116dba2de60bb905c6a37aefecd22dd
phamvandan <phamvandan.cntt2@gmail.com>
2020-06-09 14:17:47 +0700

    dan ne

--- src/main/java/mta/ltnc/BookStore/BookStoreApplication.java
+++ src/main/java/mta/ltnc/BookStore/BookStoreApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookStoreApplication {
-
+ // test dan branch commit
 	public static void main(String[] args) {
 		SpringApplication.run(BookStoreApplication.class, args);
 	}
