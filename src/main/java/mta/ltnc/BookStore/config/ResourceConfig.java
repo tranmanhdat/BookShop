cad96167dd46739d360b3b77dff146d20c5e8af0
trandat <tranmanhdathsgs@gmail.com>
2020-06-20 21:47:52 +0700

    try href without thymeleaf

--- src/main/java/mta/ltnc/BookStore/config/ResourceConfig.java
+++ src/main/java/mta/ltnc/BookStore/config/ResourceConfig.java
@@ -7,13 +7,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class ResourceConfig implements
         WebMvcConfigurer {
-    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
-            "classpath:/META-INF/resources/", "classpath:/resources/",
-            "classpath:/static/", "classpath:/public/" };
-
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
         registry.addResourceHandler("/**")
-                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+                .addResourceLocations("classpath:/static/","classpath:/templates/");
     }
 }
